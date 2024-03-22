02d62cd98fa09f5c2601467c7b70b8bef7f34b78
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-03-22 09:06:35 +0500

    feat: set use path alias for import modules

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Header } from "./components";
-import { Footer } from "./components";
+import { Header } from "@/components";
+import { Footer } from "@/components";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
