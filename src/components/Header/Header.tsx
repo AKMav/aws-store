67cc234b12b82876a412e9c0d3f9915cda13a841
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-03-25 08:25:38 +0500

    fix: remove m-ui, install bootstrap, make top-header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { Form, Container, Row, Col } from "react-bootstrap";
 import "./style.scss";
 
+enum Langs {
+  en = "English",
+  ru = "Russian",
+}
+
 export const Header = () => {
-  return <div className="app-header">Header</div>;
+  const [lang, setLang] = useState(Langs.en);
+
+  const changeLang = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value as Langs;
+    setLang(value);
+  };
+
+  return (
+    <header className="app-header">
+      <div className="top-header">
+        <Container className="top-header__container">
+          <Row>
+            <Col className="top-header__col">
+              <h2 className="top-header__text">
+                {lang}
+                Summer Sale For All Swim Suits And Free Express Delivery - OFF
+                50%!
+                <a
+                  href="#"
+                  onClick={(e) => e.preventDefault()}
+                  className="top-header__link"
+                >
+                  ShopNow
+                </a>
+              </h2>
+              <Form.Select
+                size="sm"
+                className="language-select"
+                data-bs-theme="dark"
+                onChange={changeLang}
+              >
+                <option value={Langs.en}>{Langs.en}</option>
+                <option value={Langs.ru}>{Langs.ru}</option>
+              </Form.Select>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="header-body">
+        <Container className="header-body__container">
+          <Row>Header</Row>
+        </Container>
+      </div>
+    </header>
+  );
 };
