ef91f6c6b79fcbcf9d814ee5f6b59fbcb8d055d4
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-03-26 09:41:50 +0500

    feat: add light-theme css vars

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./assets/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
