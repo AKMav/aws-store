9f28848dc0f5f49a4ec981ba712a819be3916068
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-04-16 08:36:34 +0500

    fix: add save chosen category into params

--- src/pages/HomePage/components/CategoriesList/CategoriesList.tsx
+++ src/pages/HomePage/components/CategoriesList/CategoriesList.tsx
@@ -1,17 +1,32 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button, Spinner } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
 import { fetchCategories } from "@/services/products";
 import { normalizeCategories } from "@/utils/adapters";
 import { RootState } from "@/store";
 import { updateAllCategories } from "@/store/categories";
 import "./style.scss";
 
+const CATEGORY = "category";
+
 export const CategoriesList = () => {
   const categories = useSelector((state: RootState) => state.categories.list);
   const dispatch = useDispatch();
 
   const [loading, setLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [currentCategory, setCategory] = useState("");
+  const getCategoryFromParams = () => {
+    const category = searchParams.get(CATEGORY);
+
+    if (category) {
+      setCategory(category);
+    }
+  };
+  const setCategoryToParams = (category: string) => {
+    setSearchParams({ category });
+  };
 
   useEffect(() => {
     const getCategories = async () => {
@@ -27,7 +42,10 @@ export const CategoriesList = () => {
     };
 
     getCategories();
+    getCategoryFromParams();
   }, []);
+
+  useEffect(getCategoryFromParams, [searchParams]);
 
   return (
     <div className="categories-list__wrapper">
@@ -39,8 +57,12 @@ export const CategoriesList = () => {
             <li key={category} className="categories-list__item">
               <Button
                 variant="link"
-                className="categories-list__link"
-                onClick={() => console.log(category)}
+                className={
+                  currentCategory === category
+                    ? "categories-list__link categories-list__link_active"
+                    : "categories-list__link"
+                }
+                onClick={() => setCategoryToParams(category)}
               >
                 {title}
               </Button>
