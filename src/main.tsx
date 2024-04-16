743e80511f1fbb340367d3f66b88c956d9b53113
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-04-16 10:22:14 +0500

    ref: change styles assets

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/index.scss";
+import "./assets/styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
