4b76a4aad05bfdfb99baf07eca09b7697bb188be
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-04-30 16:24:53 +0500

    feat: init add of wishlist page

---
+++ src/pages/Wishlist/Wishlist.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Wishlist = () => {
+  return (
+    <Container className="wishlist-page">
+      <h2>Wishlist title</h2>
+    </Container>
+  );
+};
+
+export default Wishlist;
