8ff6a1a3d1ec52d8304e48e7b5c9c79a8d154f29
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-04-30 14:13:25 +0500

    fix: add fix width for cart products table column, add back to shop button

--- src/pages/BuyerCart/BuyerCart.tsx
+++ src/pages/BuyerCart/BuyerCart.tsx
@@ -1,17 +1,39 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { ProductsTable, EmptyCart } from "./components";
+import { useNavigate } from "react-router-dom";
 import { RootState } from "@/store";
 import "./style.scss";
 
+const HOME_PATH = "/";
+
 const BuyerCart = () => {
   const buyerCartList = useSelector((state: RootState) => state.buyerCart.list);
+  const navigate = useNavigate();
 
   return (
     <Container>
       <div className="buyer-cart-page">
         {buyerCartList?.length ? (
-          <ProductsTable products={buyerCartList} />
+          <>
+            <ProductsTable products={buyerCartList} />
+            <div className="buyer-cart-page__row buttons-row">
+              <Button
+                variant="outline-dark"
+                className="buyer-cart-page__button"
+                onClick={() => navigate(HOME_PATH)}
+              >
+                Return To Shop
+              </Button>
+
+              <Button
+                variant="outline-dark"
+                className="buyer-cart-page__button"
+              >
+                Update Cart
+              </Button>
+            </div>
+          </>
         ) : (
           <EmptyCart />
         )}
