1b4272912baec4dec496eddae9688db8c9f70019
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-05-04 20:16:29 +0500

    fix: change structure of NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,23 +4,21 @@ import "./style.scss";
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <Container className="not-found__container">
-        <Row className="not-found__row">
-          <Col>
-            <h1 className="not-found__title">404 Not Found</h1>
-            <h3 className="not-found__subtitle">
-              Your visited page not found. You may go home page.
-            </h3>
-          </Col>
-        </Row>
-        <Row className="not-found__row">
-          <Col>
-            <BackHomeButton />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container className="not-found-page">
+      <Row className="not-found-page__row">
+        <Col>
+          <h1 className="not-found-page__title">404 Not Found</h1>
+          <h3 className="not-found-page__subtitle">
+            Your visited page not found. You may go home page.
+          </h3>
+        </Col>
+      </Row>
+      <Row className="not-found-page__row">
+        <Col>
+          <BackHomeButton />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
