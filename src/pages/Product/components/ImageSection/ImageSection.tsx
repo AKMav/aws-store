41f6699517f54306514acde0b207561ba8d7f676
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-06-21 14:04:39 +0500

    fix: make change main item picture

--- src/pages/Product/components/ImageSection/ImageSection.tsx
+++ src/pages/Product/components/ImageSection/ImageSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./style.scss";
 
 interface IProps {
@@ -6,20 +7,31 @@ interface IProps {
 }
 
 export const ImageSection = ({ mainImage, images }: IProps) => {
+  const [allImages, setAllImages] = useState<string[]>(images);
+  const [mainPic, setMainPic] = useState<string>(mainImage);
+
+  const changeMainPic = (newMainPic: string) => {
+    const oldMainPic = mainPic;
+    const images = allImages.filter((image) => image !== newMainPic);
+    setAllImages([...images, oldMainPic]);
+    setMainPic(newMainPic);
+  };
+
   return (
     <div className="full-product-images">
       <div className="full-product-images__row">
-        {images.map((imageSrc, index) => (
+        {allImages.map((imageSrc, index) => (
           <div
             key={index}
             className="full-product-images__image-container mini"
+            onClick={() => changeMainPic(imageSrc)}
           >
             <img src={imageSrc} alt="product mini image" draggable={false} />
           </div>
         ))}
       </div>
-      <div className="full-product-images__image-container">
-        <img src={mainImage} alt="product main image" draggable={false} />
+      <div className="full-product-images__image-container full-product-images_main">
+        <img src={mainPic} alt="product main image" draggable={false} />
       </div>
     </div>
   );
