4e88f6b035788e4c1808aeee9ee7200d50266e43
Akmal Mavrayev <a.mavrayev@napoleonit.ru>
2024-07-08 16:55:49 +0500

    fix: resolve jest-dom types problem, change RoundButton.test

--- src/components/shared/RoundButton/__tests__/RoundButton.test.tsx
+++ src/components/shared/RoundButton/__tests__/RoundButton.test.tsx
@@ -1,34 +1,31 @@
-import { ByRoleMatcher, render } from "@testing-library/react";
-import { RoundButton } from "./RoundButton";
-
-const NAME = "round-button-name";
-const ROLE_BUTTON: ByRoleMatcher = "button";
-const ROLE_LINK: ByRoleMatcher = "link";
+import { render } from "@testing-library/react";
+import { NAME, ROLE_BUTTON, ROLE_LINK } from "./constants";
+import { RoundButton } from "../RoundButton";
 
 describe("RoundButton testing", () => {
   it("renders the RoundButton component empty", () => {
     const { getByRole } = render(<RoundButton />);
     const button = getByRole(ROLE_BUTTON);
-    expect(button).toBeDefined();
+    expect(button).toBeInTheDocument();
   });
 
   it("renders the RoundButton component with name", () => {
     const { getByText } = render(<RoundButton>{NAME}</RoundButton>);
     const button = getByText(NAME);
-    expect(button).toBeDefined();
+    expect(button).toBeInTheDocument();
   });
 
   it("renders the RoundButton component with span", () => {
     const CHILD = <span>{NAME}</span>;
     const { getByText } = render(<RoundButton>{CHILD}</RoundButton>);
     const button = getByText(NAME);
-    expect(button).toBeDefined();
+    expect(button).toBeInTheDocument();
   });
 
   it("renders the RoundButton component with link", () => {
     const CHILD = <a href="#" />;
     const { getByRole } = render(<RoundButton>{CHILD}</RoundButton>);
     const link = getByRole(ROLE_LINK);
-    expect(link).toBeDefined();
+    expect(link).toBeInTheDocument();
   });
 });
---
+++ src/components/shared/RoundButton/__tests__/constants.ts
@@ -0,0 +1,5 @@
+import { ByRoleMatcher } from "@testing-library/react";
+
+export const NAME = "round-button-name";
+export const ROLE_BUTTON: ByRoleMatcher = "button";
+export const ROLE_LINK: ByRoleMatcher = "link";
